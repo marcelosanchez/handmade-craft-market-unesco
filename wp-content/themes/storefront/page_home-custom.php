4575197271ac9cd8902f773ff8a468926b51146e
marcelosanchez <mdsv14@gmail.com>
2017-12-17 12:58:31 -0500

    WIP Home Page Final

--- wp-content/themes/storefront/page_home-custom.php
+++ wp-content/themes/storefront/page_home-custom.php
@@ -21,8 +21,8 @@ get_header(); ?>
 			<div class="section main-home-section" id="section0">
 				<div class="intro">
 
-					<div class="col-md-12" style="text-align: center; margin-top: 8px;margin-bottom: 163px;">
-						<img style="display: inline-block;" alt="background" src="/integ.handicrafts.unesco/wp-content/uploads/img/demo_logo.png">
+					<div class="home_logo_cont col-md-12">
+						<img alt="background" src="/integ.handicrafts.unesco/wp-content/uploads/img/demo_logo.png">
 					</div>
 					<h1>Handicraft Online Shop</h1>
 					<h2>Find what you want in our variety of crafts</h2>
@@ -32,47 +32,75 @@ get_header(); ?>
 
 				</div>
 			</div>
-			<div class="section artisans-home-section" id="section1">
-				<div class="slide" id="slide1">
-					<div class="intro">
-						<div class="dummy-space" style="height: 76px; width: 100%;"></div>
-						<p style="text-align: center;">
-							<img alt="background" src="/integ.handicrafts.unesco/wp-content/uploads/img/demo_logo.png">
-						</p>
-						&nbsp;
-						<p style="text-align: left; font-size: 24px;">Artisans</p>
-						Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt egestas tempor consectetur mi. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id, egestas nulla. Ut a elit ut ipsum posuere tincidunt. Donec vitae sem id quam congue mattis non non dui.
-						<button style="margin: 0 auto;">Read More</button>
+			<div class="section artisans-home-section sub-section" id="section1">
+				<div class="intro">
+
+					<div class="home_logo_cont col-md-12">
+						<img alt="background" src="/integ.handicrafts.unesco/wp-content/uploads/img/demo_logo.png">
 					</div>
+					
+					<div class="row txtDesc_home">
+						<div class="col-md-4 offset-md-2">
+							<h1 class="">Artisans</h1>
+							<p class="">Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt egestas tempor consectetur mi. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id.</p>
+							<button class="goto-btn">Read More</button>
+						</div>
+						<div class="col-md-6">
+							<img style="" src="/integ.handicrafts.unesco/wp-content/uploads/img/home/artisans_default_img.jpg">
+						</div>
+					</div>
+					
+				</div>	
+			</div>
+			<div class="section handicraft-home-section sub-section" id="section2">
+				<div class="intro">
+
+					<div class="home_logo_cont col-md-12">
+						<img alt="background" src="/integ.handicrafts.unesco/wp-content/uploads/img/demo_logo.png">
+					</div>
+					
+					<div class="row txtDesc_home">
+						<div class="col-md-4 offset-md-2">
+							<h1 class="">Handicraft</h1>
+							<p class="">Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt egestas tempor consectetur mi. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id.</p>
+							<button class="goto-btn">Read More</button>
+						</div>
+						<div class="col-md-6">
+							<img style="" src="/integ.handicrafts.unesco/wp-content/uploads/img/home/artisans_default_img.jpg">
+						</div>
+					</div>
+
 				</div>
-				<div class="slide" id="slide2">
+			</div>
+			<div class="section cities-home-section sub-section" id="section3">
+				<div class="intro">
+
+					<div class="home_logo_cont col-md-12">
+						<img alt="background" src="/integ.handicrafts.unesco/wp-content/uploads/img/demo_logo.png">
+					</div>
+					
+					<div class="row txtDesc_home">
+						<div class="col-md-4 offset-md-2">
+							<h1 class="">Cities</h1>
+							<p class="">Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt egestas tempor consectetur mi. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id.</p>
+							<button class="goto-btn">Read More</button>
+						</div>
+						<div class="col-md-6">
+							<img style="" src="/integ.handicrafts.unesco/wp-content/uploads/img/home/artisans_default_img.jpg">
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<!-- <div class="section artisans-home-section" id="section4">
+				<div class="slide" id="slide4">
 					<h1>Slide 2</h1>
 				</div>
-			</div>
-			<div class="section handicraft-home-section" id="section2">
-				<div class="intro">
-					<div class="dummy-space" style="height: 76px; width: 100%;"></div>
-					<p style="text-align: center;">
-						<img alt="background" src="/integ.handicrafts.unesco/wp-content/uploads/img/demo_logo.png">
-					</p>
-					&nbsp;
-					<p style="text-align: left; font-size: 24px;">Handcraft</p>
-					Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt egestas tempor consectetur mi. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id, egestas nulla. Ut a elit ut ipsum posuere tincidunt. Donec vitae sem id quam congue mattis non non dui.
-					<button style="margin: 0 auto;">Read More</button>
+				<div class="slide" id="slide5">
+					<h1>Slide 2</h1>
 				</div>
-			</div>
-			<div class="section cities-home-section" id="section3">
-				<div class="intro">
-					<div class="dummy-space" style="height: 76px; width: 100%;"></div>
-					<p style="text-align: center;">
-						<img alt="background" src="/integ.handicrafts.unesco/wp-content/uploads/img/demo_logo.png">
-					</p>
-					&nbsp;
-					<p style="text-align: left; font-size: 24px;">Cities</p>
-					Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt egestas tempor consectetur mi. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id, egestas nulla. Ut a elit ut ipsum posuere tincidunt. Donec vitae sem id quam congue mattis non non dui.
-					<button style="margin: 0 auto;">Read More</button>
-				</div>
-			</div>
+			</div> -->
 		</div>
 
 	</main><!-- #main -->
