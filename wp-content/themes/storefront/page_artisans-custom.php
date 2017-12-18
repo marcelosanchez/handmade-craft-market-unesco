f6343547771d5d81463d077e45c5bf343487db08
marcelosanchez <mdsv14@gmail.com>
2017-12-17 23:39:21 -0500

    WIP Artisans Page 2

--- wp-content/themes/storefront/page_artisans-custom.php
+++ wp-content/themes/storefront/page_artisans-custom.php
@@ -36,9 +36,78 @@ get_header(); ?>
 				<p>Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue.</p>
 			</div>
 		</div>
+		<div class="artisans_h_cont">
+			<h1 class="text-center">Meet our artisans</h1>
+			<h2 class="text-center">Excellent artisans from different countries</h2>
+		</div>
 
-		<h1 class="text-center"><b>Meet our artisans</b></h1>
-		<h2 class="text-center">Excellent artisans from different countries</h2>
+		<div class="artisans_main_cont row">
+			
+			<div class="card col-md-3 col-md-offset-1">
+				<div class="card-header row">
+					<div class="user_cont col-md-12">
+						<img class="avatar-idArtisan" src="/integ.handicrafts.unesco/wp-content/uploads/img/artisans/artisan_id_img/artisan-id01.jpg" alt="Avatar">
+						<div class="user_data col-md-8">
+							<span class="row" title="Full Name" href="#">
+								<b>Artisan Name</b>
+							</span>
+							<span class="row">Artisan Country</span>
+						</div>
+					</div>
+				</div>
+				<div class="card-avatar-img row">
+				</div>
+				<div class="card-body row">
+					<p>Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue.</p>
+					<button class="goto-btn">Read More</button>
+				</div>
+			</div>
+
+			<div class="card col-md-3 col-md-offset-1">
+				<div class="card-header row">
+					<div class="user_cont col-md-12">
+						<img class="avatar-idArtisan" src="/integ.handicrafts.unesco/wp-content/uploads/img/artisans/artisan_id_img/artisan-id01.jpg" alt="Avatar">
+						<div class="user_data col-md-8">
+							<span class="row" title="Full Name" href="#">
+								<b>Artisan Name</b>
+							</span>
+							<span class="row">Artisan Country</span>
+						</div>
+					</div>
+				</div>
+				<div class="card-avatar-img row">
+				</div>
+				<div class="card-body row">
+					<p>Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue.</p>
+					<button class="goto-btn">Read More</button>
+				</div>
+			</div>
+
+			<div class="card col-md-3 col-md-offset-1">
+				<div class="card-header row">
+					<div class="user_cont col-md-12">
+						<img class="avatar-idArtisan" src="/integ.handicrafts.unesco/wp-content/uploads/img/artisans/artisan_id_img/artisan-id01.jpg" alt="Avatar">
+						<div class="user_data col-md-8">
+							<span class="row" title="Full Name" href="#">
+								<b>Artisan Name</b>
+							</span>
+							<span class="row">Artisan Country</span>
+						</div>
+					</div>
+				</div>
+				<div class="card-avatar-img row">
+				</div>
+				<div class="card-body row">
+					<p>Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue.</p>
+					<button class="goto-btn">Read More</button>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="view_more_artisans row text-center">
+			<button class="goto-btn" style="margin: 0 auto;">View More</button>
+		</div>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
