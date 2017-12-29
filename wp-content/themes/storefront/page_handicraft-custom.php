40f2fd9f25bb65328696923f7da3a0c9f7e95e43
marcelosanchez <mdsv14@gmail.com>
2017-12-29 16:30:21 -0500

    WIP Handicrafts page

--- wp-content/themes/storefront/page_handicraft-custom.php
+++ wp-content/themes/storefront/page_handicraft-custom.php
@@ -14,14 +14,28 @@
 
 get_header(); ?>
 
+<!-- ARTISANS PAGE -->
+<link rel="stylesheet" type="text/css" href="/integ.handicrafts.unesco/wp-content/uploads/css/page_handicraft/handicrafts_page.css" />
+
+<script type="text/javascript" src="/integ.handicrafts.unesco/wp-content/uploads/js/page_handicraft/handicrafts_page.js"></script>
+<!-- EO / ARTISANS PAGE -->
+
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
 		<div class="col-md-12 gPage_hCont parallax">
-			Handicraft
+			<?php echo get_the_title(); ?>
 		</div>
 
-		<span>Handicraft Content</span>
+		<div class="row txtDesc_general">
+			<div class="col-md-6">
+				<img style="" src="/integ.handicrafts.unesco/wp-content/uploads/img/artisans/artisans_general.jpg">
+			</div>
+			<div class="col-md-4">
+				<p class="text-left">Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue.</p> 
+				<p>Donec a gravida ipsum. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue. Etiam iaculis mauris quis nulla tincidunt. Nullam ullamcorper posuere nisl, quis commodo purus fringilla in. Mauris ultricies massa nisl, id mollis turpis gravida ut. Aliquam erat volutpat. Aliquam id metus ultrices, eleifend ipsum id. Nunc magna nibh, pulvinar vehicula velit at, convallis volutpat augue.</p>
+			</div>
+		</div>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
---
+++ wp-content/uploads/js/page_handicraft/handicrafts_page.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	// Code Here
+	
+	// var lastScrollTop = 0;
+	// $(window).scroll(function(event){
+	//    var st = $(this).scrollTop();
+	//    if (st > lastScrollTop){
+	//    		// Down
+	//        $('html, body').animate({
+	//        	scrollTop: $("#meet_ur_artisans").offset().top - 68
+	//     		}, 2000);
+	//    } else {
+	//    		// Up
+	//       	$('html, body').animate({
+	//        	scrollTop: $("#primary").offset().top - 110
+	//     		}, 2000);
+	//    }
+	//    console.log(st);
+	//    console.log(lastScrollTop);
+	//    lastScrollTop = st;
+	// });
+
+	// $(window).bind('mousewheel', function(event) {
+	//        if (event.originalEvent.wheelDelta >= 0) {
+	//            // $(window).scrollTo('0%', 1000);
+	//            // Up
+	// 	      	$('html, body').animate({
+	// 	       	scrollTop: $("#primary").offset().top - 110
+	// 	    		}, 2000);
+	//        }
+	//        else {
+	//            // $(window).scrollTo('100%', 1000);
+	//            // Down
+	// 	       $('html, body').animate({
+	// 	       	scrollTop: $("#meet_ur_artisans").offset().top - 68
+	// 	    		}, 2000);
+	//        }
+	//        event.preventDefault();
+	//    var st = $(this).scrollTop();
+
+	//    console.log(st);
+	       
+	//    });
+});
